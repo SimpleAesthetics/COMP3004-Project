ebad4f6616c5bdbfa784fdab3992eaef1720962e
Advent1013 <Vishahan@users.noreply.github.com>
2017-10-18 12:20:59 -0400

    Finally Push to GitHub?
    Just seeing if this will work.

--- Grouper/app/src/main/java/com/aesthetics/simple/grouper/MainLists.java
+++ Grouper/app/src/main/java/com/aesthetics/simple/grouper/MainLists.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
@@ -25,6 +27,7 @@ public class MainLists extends AppCompatActivity {
     private SimpleAdapter adapter;
     private String title;
     private ListView lst_Main;
+    private PopupMenu popup;
 
     //Lists;
     private List<Map<String, String>> lst_Universities;
@@ -45,6 +48,8 @@ public class MainLists extends AppCompatActivity {
         btn_search = (ImageButton) findViewById(R.id.btn_search);
         btn_search.setImageResource(R.drawable.ic_search);
 
+        popup = new PopupMenu(this,btn_Menu);
+        popup.getMenu().add("Exit");
         //Initialize title;
         txt_Title = (TextView) findViewById(R.id.txt_Title);
         txt_Title.setText("Unknown");
@@ -52,6 +57,21 @@ public class MainLists extends AppCompatActivity {
         //Set list content;
         lst_Main = (ListView) findViewById(R.id.lst_Main);
         lst_Main.setAdapter(adapter);
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                finish();
+                return false;
+            }
+        });
+
+        btn_Menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popup.show();
+            }
+        });
 
         view_Universities();
     }
@@ -76,13 +96,13 @@ public class MainLists extends AppCompatActivity {
     }
 
     public void view_Universities(){
-        txt_Title.setText("  Universities");
+        txt_Title.setText("    Universities");
         btn_Back.setVisibility(View.GONE);
         lst_Universities= new ArrayList<Map<String, String>>();
         //Hard Coded universities list;
-        lst_Universities.add(newListItem("Carleton University","Ottawa ON"));
-        lst_Universities.add(newListItem("University of Ottawa","Ottawa ON"));
-        lst_Universities.add(newListItem("University of Toronto","Toronto ON"));
+        lst_Universities.add(newListItem("Carleton University","Ottawa ON\n3 Courses"));
+        lst_Universities.add(newListItem("University of Ottawa","Ottawa ON\n4 Courses"));
+        lst_Universities.add(newListItem("University of Toronto","Toronto ON\n1 Course"));
 
         set_List(lst_Universities);
         lst_Main.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -103,11 +123,33 @@ public class MainLists extends AppCompatActivity {
                 view_Universities();
             }
         });
+        lst_Courses= new ArrayList<Map<String, String>>();
+        set_List(lst_Courses);
+        lst_Courses.add(newListItem("COMP3001","4 Completed Environments\n3 Pending Environments"));
+        lst_Courses.add(newListItem("COMP3004","3 Completed Environments\n0 Pending Environments"));
 
+        lst_Main.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Get Courses from here; using on click id.
+                view_Environments();
+            }
+        });
     }
 
     public void view_Environments(){
-
+        txt_Title.setText("Environment");
+        btn_Back.setVisibility(View.VISIBLE);
+        btn_Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view_Courses();
+            }
+        });
+        lst_Environments= new ArrayList<Map<String, String>>();
+        set_List(lst_Environments);
+        lst_Environments.add(newListItem("Fall Project","3 Waiting. 32 Groups.\nEnds 19 October 2017"));
+        lst_Environments.add(newListItem("Study Groups","14 Waiting. 3 Groups.\nDue date reached."));
     }
 
     public void view_Groups(){
