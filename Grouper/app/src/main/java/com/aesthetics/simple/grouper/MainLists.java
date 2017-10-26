b72360bc4365f2c7e9b5448156cc8fca8ea2467c
Advent1013 <Vishahan@users.noreply.github.com>
2017-10-26 10:37:52 -0400

    More UI Work

--- Grouper/app/src/main/java/com/aesthetics/simple/grouper/MainLists.java
+++ Grouper/app/src/main/java/com/aesthetics/simple/grouper/MainLists.java
@@ -1,11 +1,8 @@
 package com.aesthetics.simple.grouper;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PopupMenu;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,9 +11,9 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,12 +25,19 @@ public class MainLists extends AppCompatActivity {
     private String title;
     private ListView lst_Main;
     private PopupMenu popup;
+    private int state = 0;
+    private int univ,cour,envi,grou;
 
     //Lists;
     private List<Map<String, String>> lst_Universities;
     private List<Map<String, String>> lst_Courses;
     private List<Map<String, String>> lst_Environments;
     private List<Map<String, String>> lst_Inner;
+
+    private List<Integer> lst_ID_Universities;
+    private List<Integer> lst_ID_Courses;
+    private List<Integer> lst_ID_Environments;
+    private List<Integer> lst_ID_Inner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,7 +77,15 @@ public class MainLists extends AppCompatActivity {
             }
         });
 
-        view_Universities();
+        view_Universities(true);
+
+        btn_Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backAStep();
+            }
+        });
+
     }
 
     private void set_List(List<Map<String, String>> lst_cur){
@@ -88,6 +100,52 @@ public class MainLists extends AppCompatActivity {
 
     }
 
+    private String newUniversityListItem(String Name, int num){
+        String out = Name + "\n" + num + " available course";
+        if (num!=1) out=out+"s";
+        return out;
+    }
+
+    private String newCourseListItem(String Name, int CompNum, int pendNum){
+        String out = Name + "\n" + CompNum + " Completed Environment";
+        if (CompNum!=1) out+="s\n";
+        out+=pendNum+" Incomplete Environment";
+        if (pendNum!=1) out+="s";
+        return out;
+    }
+
+    private String newEnvironmentListItem(String Name, int groupNum, int membNum, boolean formed, Date dueDate){
+        String out = Name+"\n";
+        if (formed){
+            out+=groupNum+" Group";
+            if (groupNum!=1) out+="s";
+            out+=" Formed\n"+membNum+" Member";
+            if (membNum!=1) out+="s";
+        }
+        else{
+            out+=membNum+" Member";
+            if (membNum!=1) out+="s\nDeadline: "+dueDate.toString();
+        }
+        return out;
+    }
+
+    private String newPeopleListItem(String Name, float match, boolean linked){
+        String out = Name+"\n" + match + "% Match"+(linked?"\nLINKED":"");
+        return out;
+    }
+
+    private void backAStep(){
+        if(state==0)
+            finish();
+        else if(state==1)
+            view_Universities(false);
+        else if(state==2)
+            view_Courses(false);
+        if (state==3)
+            view_Environments(false);
+
+    }
+
     private Map<String, String> newListItem(String H1, String Sub){
         Map<String, String> data = new HashMap<String, String>(2);
         data.put("H1",H1);
@@ -95,74 +153,86 @@ public class MainLists extends AppCompatActivity {
         return data;
     }
 
-    public void view_Universities(){
+    public void view_Universities(boolean reload){
+        state=0;
         txt_Title.setText("    Universities");
         btn_Back.setVisibility(View.GONE);
-        lst_Universities= new ArrayList<Map<String, String>>();
-        //Hard Coded universities list;
-        lst_Universities.add(newListItem("Carleton University","Ottawa ON\n3 Courses"));
-        lst_Universities.add(newListItem("University of Ottawa","Ottawa ON\n4 Courses"));
-        lst_Universities.add(newListItem("University of Toronto","Toronto ON\n1 Course"));
+        if(reload) {
+            lst_Universities    = new ArrayList<Map<String, String>>();
+            lst_ID_Universities = new ArrayList<Integer>();
+            //Get universities;
+        }
 
         set_List(lst_Universities);
         lst_Main.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //Get Courses from here; using on click id.
-                view_Courses();
+                univ = lst_ID_Universities.get((int)position);
+                view_Courses(true);
             }
         });
     }
 
-    public void view_Courses(){
+    public void view_Courses(boolean reload){
+        state=1;
         txt_Title.setText("Courses");
         btn_Back.setVisibility(View.VISIBLE);
-        btn_Back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                view_Universities();
-            }
-        });
-        lst_Courses= new ArrayList<Map<String, String>>();
+        if(reload) {
+            lst_Courses     = new ArrayList<Map<String, String>>();
+            lst_ID_Courses  = new ArrayList<Integer>();
+        }
         set_List(lst_Courses);
-        lst_Courses.add(newListItem("COMP3001","4 Completed Environments\n3 Pending Environments"));
-        lst_Courses.add(newListItem("COMP3004","3 Completed Environments\n0 Pending Environments"));
 
         lst_Main.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //Get Courses from here; using on click id.
-                view_Environments();
+                cour=lst_ID_Courses.get((int)position);
+                view_Environments(true);
             }
         });
     }
 
-    public void view_Environments(){
-        txt_Title.setText("Environment");
+    public void view_Environments(boolean reload){
+        state=2;
+        txt_Title.setText("Environments");
         btn_Back.setVisibility(View.VISIBLE);
-        btn_Back.setOnClickListener(new View.OnClickListener() {
+        if(reload) {
+            lst_ID_Environments = new ArrayList<Integer>();
+            lst_Environments    = new ArrayList<Map<String, String>>();
+        }
+        set_List(lst_Environments);
+        lst_Main.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                view_Courses();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Get Courses from here; using on click id.
+                envi=lst_ID_Environments.get((int)position);
+                view_People();
             }
         });
-        lst_Environments= new ArrayList<Map<String, String>>();
-        set_List(lst_Environments);
-        lst_Environments.add(newListItem("Fall Project","3 Waiting. 32 Groups.\nEnds 19 October 2017"));
-        lst_Environments.add(newListItem("Study Groups","14 Waiting. 3 Groups.\nDue date reached."));
-    }
-
-    public void view_Groups(){
-
     }
 
     public void view_People(){
-
+        state=3;
+        txt_Title.setText("People");
+        btn_Back.setVisibility(View.VISIBLE);
+        lst_ID_Environments = new ArrayList<Integer>();
+        lst_Environments    = new ArrayList<Map<String, String>>();
+        set_List(lst_Environments);
+        lst_Main.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Get Courses from here; using on click id.
+                envi=lst_ID_Inner.get((int)position);
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
         //Include the code here
+        backAStep();
         return;
     }
 }
